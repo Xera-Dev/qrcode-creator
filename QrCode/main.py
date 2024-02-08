ad0d4f6e0ffa3ff01f10726ff9283ace2ccdbf7c
Xera-Dev <86786081+Xera-Dev@users.noreply.github.com>
2024-02-08 19:04:53 +0100

    Update main.py

--- QrCode/main.py
+++ QrCode/main.py
@@ -1,5 +1,5 @@
-import segno
+import segno  #import library
 
-lien = input("Entrez le lien du Qr Code: ")
+lien = input("Entrez le lien du Qr Code: ") #Qr Code link
 qrcode = segno.make_qr(lien)
-qrcode.save('qr.png', scale=10)
+qrcode.save('qr.png', scale=10) #Qr Code generation  name: "qr.png"
